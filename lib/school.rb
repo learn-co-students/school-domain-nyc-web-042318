cd1f4cf14323ad372adae7e26e9bbad6dd9cacef
github username <torpid@gmail.com>
2018-04-26 13:30:24 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
-# code here!
+require "pry"
+
+class School
+
+attr_accessor :school, :student, :grade, :roster
+
+def initialize(roster)
+  @roster = {}
+end
+
+def add_student(student, grade)
+  if @roster.key?(grade) == false
+    @roster[grade] = []
+    @roster[grade] << student
+  else
+    @roster[grade] << student
+  end
+end
+
+def grade(grade)
+  @roster[grade]
+end
+
+def sort
+  @roster.each do |grade, student_array|
+  @roster[grade] = student_array.sort
+  end
+end
+end
