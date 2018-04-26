73d8d007ddbf843016c532109d5035be2b941d4c
Derek Chen <derek.g.chen@gmail.com>
2018-04-26 00:37:45 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,10 +1,32 @@
 # code here!
 class School
-  attr_accessor :
+  attr_accessor :roster
   attr_reader :name
 
   def initialize(name)
     @name = name
+    @roster = {}
+
+  end
+
+  def add_student(student,grade)
+    if(!@roster.has_key?(grade))
+      @roster[grade] =[]
+    end
+    @roster[grade] << student
+  end
+
+  def grade(n)
+    @roster[n]
+
+  end
+
+  def sort
+    temp= {}
+   @roster.each do |k,v|
+      temp[k] = v.sort
+    end
+    temp
 
   end
 
